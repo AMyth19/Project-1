f4e96c0d990c222594079368cdf0e1da8b637147
AMyth19 <amitkanani09@gmail.com>
2023-12-01 02:51:10 +0530

    changed to FB

--- src/session/Google.java
+++ src/session/Google.java
@@ -1,9 +1,20 @@
 package session;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 
 public class Google {
-	System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver_win32\\chromedriver");
+	public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
-	driver.get("https://www.flipkart.com/");
-
+	driver.get("https://www.facebook.com/");
+	driver.manage().window().maximize();
+	WebElement EmailAddress=driver.findElement(By.id("email"));
+	EmailAddress.sendKeys("amitkanani09@gmail.com");
+	WebElement Password=driver.findElement(By.id("pass"));
+	Password.sendKeys("Password123$");
+	WebElement Login=driver.findElement(By.name("login"));
+	Login.click();
+	}
 }
