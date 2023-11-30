d93285d2e9b2acb244bb4eddf513ecd1c0caf1c4
AMyth19 <amitkanani09@gmail.com>
2023-11-30 23:36:20 +0530

    Project Setup

---
+++ src/session/Google.java
@@ -0,0 +1,9 @@
+package session;
+
+
+public class Google {
+	System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver_win32\\chromedriver");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.flipkart.com/");
+
+}
